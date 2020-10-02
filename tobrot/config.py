c051a00f3817a2e0f170690a8625354bee28f3f2
burungkokdi <72206270+burungkokdi@users.noreply.github.com>
2020-10-02 22:41:57 +0800

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,12 @@
+from tobrot.sample_config import Config
+#Fill your all data, read readme for reference
+
+class Config(Config):
+	TG_BOT_TOKEN= "1255639276:AAHFcNS-rLRox3hAHhT5eprESwIRWZ-OIQA" #aoitorbot
+	APP_ID = 1649008
+	API_HASH = "933aae7abdc0b82fffccf3d69d1e7a91"
+	OWNER_ID = "1282345589" #ID of bot owner
+	AUTH_CHANNEL = [-1001495399151]
+	DESTINATION_FOLDER = "botbaru" #Name of your folder read readme
+	FINISHED_PROGRESS_STR = "⬛"
+	UN_FINISHED_PROGRESS_STR = "⬜"
